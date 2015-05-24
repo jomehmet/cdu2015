4b159a97ca3d130c24f609ac130af45fb6b7a5f9
jomehmet <jomehmet.sollihagen@komplett.com>
2015-05-24 21:06:02 +0200

    Init generated web-app

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cdu2015App')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
